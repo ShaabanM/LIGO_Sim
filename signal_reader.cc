1f05272fe0f03e68414177149ed7a65acacddbf4
M Shaaban <m.shaaban@mail.utoronto.ca>
2019-03-04 21:43:35 -0500

    tested and functional basic signal netcdf file reading module written

--- signal_reader.cc
+++ signal_reader.cc
@@ -5,4 +5,13 @@ rarray<complex<double>, 1> read_signal(char *path)
     netCDF::NcFile file(path, netCDF::NcFile::read);
     rarray<std::complex<double>, 1> f(file.getDim("nt").getSize());
     file.getVar("f").getVar(&f[0]);
+
+    return f;
+}
+
+int main()
+{
+    rarray<complex<double>, 1> sig  = read_signal("gwdata/GWprediction.nc");
+
+    cout << sig[0] << endl;
 }
