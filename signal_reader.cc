555a6312de4612a5b2600712192e975847be1c97
M Shaaban <m.shaaban@mail.utoronto.ca>
2019-03-04 22:16:58 -0500

    final signal reader commit, comments added ready to begin developing the next module

--- signal_reader.cc
+++ signal_reader.cc
@@ -1,25 +1,19 @@
 #include "signal_reader.h"
 
+// Implementation of the netcdf ligo signal reader function.
 ligo_signal read_signal(char *path)
 {
-    ligo_signal sig;
-
+    // open the netcdf file in read mode and store the content as rarrays
     netCDF::NcFile file(path, netCDF::NcFile::read);
     rarray<std::complex<double>, 1> d(file.getDim("nt").getSize());
     rarray<double, 1> t(file.getDim("nt").getSize());
     file.getVar("f").getVar(&d[0]);
     file.getVar("t").getVar(&t[0]);
+
+    // store the rarrays in s ligo signal struct
+    ligo_signal sig;
     sig.data = d;
     sig.time = t;
 
     return sig;
-}
-
-int main()
-{
-    ligo_signal sig = read_signal("gwdata/GWprediction.nc");
-
-    cout << sig.data[0] << endl;
-    cout << sig.time[0] << endl;
-    cout << sig.time[1] << endl;
 }
--- signal_reader.h
+++ signal_reader.h
@@ -5,6 +5,7 @@
 #include <complex>
 using namespace std;
 
+// struct that will hold ligo signal data form netcdf files. data will hold the complex numbers and time will hold the times
 struct ligo_signal
 {
     rarray<complex<double>, 1> data;
@@ -13,6 +14,9 @@ struct ligo_signal
 
 // Function that reads a netcdf simulated LIGO datafile and writes
 // it to an rarray.
+//
+// Parameters:
+// path char array describing the path of the netcdf file to be read.
 ligo_signal read_signal(char *path);
 
 #endif // !READER
