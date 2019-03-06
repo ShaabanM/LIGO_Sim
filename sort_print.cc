29dbe00be42c477bbede28116a0b3e39cdbd3534
M Shaaban <m.shaaban@mail.utoronto.ca>
2019-03-06 17:52:57 -0500

    final version needs commenting before submission

--- sort_print.cc
+++ sort_print.cc
@@ -1,16 +1,35 @@
 #include "sort_print.h"
 #include "signal_reader.h"
 
+struct coff
+{
+    int i;
+    double C;
+};
+
+bool compare(coff c1, coff c2)
+{
+    return c1.C > c2.C;
+}
+
 void sort_print(rarray<double, 1> arr)
 {
+    rarray<coff, 1> coffs(arr.size());
 
-    std::sort(arr.begin(), arr.end(), std::greater<double>());
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        coff c;
+        c.i = i;
+        c.C = arr[i];
+        coffs[i] = c;
+    }
 
+    std::sort(coffs.begin(), coffs.end(), compare);
 
     for (size_t i = 0; i < 5; i++)
     {
-        std::string path = get_path(ind[i]);
+        std::string path = get_path(coffs[i].i);
 
-        cout << path << " has C = " << arr[i] << endl;
+        cout << path << " has C = " << coffs[i].C << endl;
     }
 }
