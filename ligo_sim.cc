f525457c3f07cf53de2bb1d5af17895f3dda6a84
M Shaaban <m.shaaban@mail.utoronto.ca>
2019-03-06 16:45:10 -0500

    fully functioning code, coefficents need to be sorted

--- ligo_sim.cc
+++ ligo_sim.cc
@@ -2,6 +2,7 @@
 #include "signal_power.h"
 #include "signal_reader.h"
 #include "correlator.h"
+#include "sort_print"
 
 using namespace std;
 
@@ -10,16 +11,28 @@ int main()
     int nd = 32;
 
     ligo_signal sig_p = read_signal("gwdata/GWprediction.nc");
+    std::string path;
+    rarray<double, 1> Cs(nd);
 
     for (size_t i = 0; i < nd; i++)
     {
-
-        std::string path = "gwdata/detection";
-        path += std::to_string(i + 1);
-        path += ".nc";
+        if (i < 9)
+        {
+            path = "gwdata/detection0";
+            path += std::to_string(i + 1);
+            path += ".nc";
+        }
+        else
+        {
+            path = "gwdata/detection";
+            path += std::to_string(i + 1);
+            path += ".nc";
+        }
 
         ligo_signal sig = read_signal(path);
         double C = correlate(power_spectrum(signal_ft(sig_p)), power_spectrum(signal_ft(sig)));
-        cout << C << endl;
+        Cs[i] = C;
     }
+
+    sort_print(Cs);
 }
