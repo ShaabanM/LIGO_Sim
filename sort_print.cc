da8cd54b9bfe8881951d274de6cc7120f1892da2
M Shaaban <m.shaaban@mail.utoronto.ca>
2019-03-07 07:05:12 -0500

    final submission completed, implmented sort and print functionality

--- sort_print.cc
+++ sort_print.cc
@@ -1,35 +1,42 @@
 #include "sort_print.h"
 #include "signal_reader.h"
 
-struct coff
-{
-    int i;
-    double C;
-};
-
-bool compare(coff c1, coff c2)
-{
-    return c1.C > c2.C;
-}
-
+// implementation of sort and print function
 void sort_print(rarray<double, 1> arr)
 {
     rarray<coff, 1> coffs(arr.size());
 
     for (size_t i = 0; i < arr.size(); i++)
     {
-        coff c;
-        c.i = i;
-        c.C = arr[i];
-        coffs[i] = c;
+        coff temp;
+        temp.i = i;
+        temp.c = arr[i];
+        coffs[i] = temp;
     }
 
     std::sort(coffs.begin(), coffs.end(), compare);
+    report(coffs);
+}
 
+// helper functions
+
+// implementation of "greater" compare function for coff struct
+bool compare(coff c1, coff c2)
+{
+    return c1.c > c2.c;
+}
+
+// implmentation of reporting the results method
+void report(rarray<coff, 1> coffs)
+{
+    cout << "The top 5 in order are:" << endl;
     for (size_t i = 0; i < 5; i++)
     {
-        std::string path = get_path(coffs[i].i);
-
-        cout << path << " has C = " << coffs[i].C << endl;
+        cout << i + 1 << ") Data set #" << coffs[i].i + 1 << " with C = " << coffs[i].c << endl;
+    }
+    cout << "===========The rest============" << endl;
+    for (size_t i = 5; i < coffs.size(); i++)
+    {
+        cout << i + 1 << ") Data set #" << coffs[i].i + 1 << " with C = " << coffs[i].c << endl;
     }
 }
--- sort_print.h
+++ sort_print.h
@@ -5,6 +5,16 @@
 #include <iostream>
 using namespace std;
 
+struct coff
+{
+    int i;
+    double c;
+};
+
 void sort_print(rarray<double, 1> arr);
+
+bool compare(coff c1, coff c2);
+
+void report(rarray<coff, 1> coffs);
 
 #endif // !SORTP
